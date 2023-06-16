98472ad1b657079af1fd27ec7fb2c77cf9ea4ab3
myplc <plctohmi@gmail.com>
2023-06-16 16:10:15 +0900

    정리추가

---
+++ nodejs/4ex_form1_GET/4ex_form1.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const app = express()
+const PORT = 3000
+const _path = path.join(__dirname, './4ex_dist')
+console.log(_path)
+app.use('/main', express.static(_path))
+app.use(logger('tiny'))
+app.get('/main/data',(req,res)=>{
+    const name = req.query.name
+    const age = req.query.age
+    const content = req.query.content
+    console.log(name, age, content)
+    res.redirect('/main/done.html')
+
+})
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`너의 서버는? ${PORT}!`)
+})
