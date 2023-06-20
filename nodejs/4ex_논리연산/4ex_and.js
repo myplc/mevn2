dafe06a77231b353e1b0d63e6072ce0553051cb1
myplc <plctohmi@gmail.com>
2023-06-20 09:15:21 +0900

    vu02수업

---
+++ nodejs/4ex_논리연산/4ex_and.js
@@ -0,0 +1,7 @@
+const log = console.log
+
+log(false&&false) 
+log(true&&false&&console.log('넌날미워할수없어'))
+log(true&&console.log('넌날미워할수없어')&&true)
+log(false&&333)
+log(true&&123)
---
+++ nodejs/4ex_논리연산/4ex_or.js
@@ -0,0 +1,7 @@
+/* boolean 논리 연산 */ 
+// console.log(false||false||false||true||false)  // ===>>
+// console.log(null||undefined||0||false||0)
+// console.log(false||false||0||-2||false)
+// console.log(true||false)
+console.log(false||console.log('야이나쁜놈아'))
+// console.log(true||console.log('야이나쁜놈아'))
