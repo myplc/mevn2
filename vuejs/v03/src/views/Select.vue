f1317969eeecb0b73ec0cda56829d5c7128273dc
myplc <plctohmi@gmail.com>
2023-06-22 13:01:19 +0900

    1

--- vuejs/v03/src/views/Select.vue
+++ vuejs/v03/src/views/Select.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
     <select name="" id="" v-model="selData">
+      <option v-for="city in Object.keys(obj)" :key="city" :value="obj[city]">
+        {{ city }}
+      </option>
+    </select>
+    <span>{{ selData }}</span>
+  </div>
+  <div>
+    <select name="" id="" v-model="selData2">
       <option v-for="city in arr" :key="city" :value="city.code">
         {{ city.name }}
       </option>
     </select>
-    <span>{{ selData }}</span>
+    <span>{{ selData2 }}</span>
   </div>
 </template>
 <script>
@@ -13,12 +21,20 @@ export default {
   data() {
     return {
       selData: '고르세요.',
+      selData2: '고르세요2.',
+      obj: {
+        부산: '051',
+        서울: '02',
+        제주: '064',
+        창원: '055',
+        대구: '053'
+      },
       arr: [
-        { name: '부산', code: '051' },
-        { name: '서울', code: '02' },
-        { name: '제주', code: '064' },
-        { name: '창원', code: '055' },
-        { name: '대구', code: '053' }
+        { name: '부산2', code: '051' },
+        { name: '서울2', code: '02' },
+        { name: '제주2', code: '064' },
+        { name: '창원2', code: '055' },
+        { name: '대구2', code: '053' }
       ]
     }
   }
