0201d51e7cb9f56687acd0aa6010d47c0ac9abb0
myplc <plctohmi@gmail.com>
2023-06-22 13:00:05 +0900

    수정

--- vuejs/v03/src/views/Select.vue
+++ vuejs/v03/src/views/Select.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <select name="" id="" v-model="selData">
-      <option v-for="city in arr" :key="city" :value="city.code">
+      <option v-for="city in arr2" :key="city" :value="city.code">
         {{ city.name }}
       </option>
     </select>
     <span>{{ selData }}</span>
+    </div><div>
+    <select name="" id="" v-model="selData2">
+      <option v-for="city in Object.keys(arr1)" :key="city" :value="arr1[city]">
+        {{city }}
+      </option>
+    </select>
+    <span>{{ selData2 }}</span>
   </div>
 </template>
 <script>
@@ -13,7 +20,15 @@ export default {
   data() {
     return {
       selData: '고르세요.',
-      arr: [
+      selData2: '고르세요2.',
+      arr1: {
+        부산: '051',
+        서울: '02',
+        제주: '064',
+        창원: '055',
+        대구: '053'
+      },
+      arr2: [
         { name: '부산', code: '051' },
         { name: '서울', code: '02' },
         { name: '제주', code: '064' },
