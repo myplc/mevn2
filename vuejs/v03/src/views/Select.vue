bd0f6bd72ef94bdec66c893dac8a3814ac3d4b16
myplc <plctohmi@gmail.com>
2023-06-22 12:09:33 +0900

    수정중

---
+++ vuejs/v03/src/views/Select.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <select name="" id="" v-model="selData">
+      <option v-for="city in arr" :key="city" :value="city.code">
+        {{ city.name }}
+      </option>
+    </select>
+    <span>{{ selData }}</span>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      selData: '고르세요.',
+      arr: [
+        { name: '부산', code: '051' },
+        { name: '서울', code: '02' },
+        { name: '제주', code: '064' },
+        { name: '창원', code: '055' },
+        { name: '대구', code: '053' }
+      ]
+    }
+  }
+}
+</script>
+<style></style>
